15ee7250abd0e972f1e1669ce98dc6d1c2657c75
Aissamdev2 <shiranui201103@gmail.com>
2025-02-03 16:40:49 +0100

    rollback to absolute

--- app/gemif/layout.tsx
+++ app/gemif/layout.tsx
@@ -4,7 +4,7 @@ export default function GemifLayout({ children }: { children: React.ReactNode })
   return (
     <>
       <Header />
-      <main className="h-full fixed top-0 left-0 w-screen bg-[#eaf3ff]">
+      <main className="h-full absolute top-0 left-0 w-screen bg-[#eaf3ff]">
         {children}
       </main>
     </>
