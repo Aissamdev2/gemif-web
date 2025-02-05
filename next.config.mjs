7cc648d1d3f1026ba51180c03c3f672644bfc2c5
Aissamdev2 <shiranui201103@gmail.com>
2025-02-05 20:57:56 +0100

    remove nextconfig contents

--- next.config.mjs
+++ next.config.mjs
@@ -1,26 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  async headers() {
-    return [
-      {
-        source: '/(.*)', // Matches all routes
-        headers: [
-          {
-            key: 'Cache-Control',
-            value: 'no-cache, no-store, must-revalidate',
-          },
-          {
-            key: 'ETag',
-            value: '', // Remove ETag header
-          },
-          {
-            key: 'Last-Modified',
-            value: '', // Remove Last-Modified header
-          },
-        ],
-      },
-    ];
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
