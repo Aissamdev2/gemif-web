c1d5e086653a80a08176fe233f22683b86d5c2e1
Aissamdev2 <shiranui201103@gmail.com>
2025-02-05 20:45:07 +0100

    remove etags and cache from everything nextconfig

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,14 @@ const nextConfig = {
             key: 'Cache-Control',
             value: 'no-cache, no-store, must-revalidate',
           },
+          {
+            key: 'ETag',
+            value: '', // Remove ETag header
+          },
+          {
+            key: 'Last-Modified',
+            value: '', // Remove Last-Modified header
+          },
         ],
       },
     ];
