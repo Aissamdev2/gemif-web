373bd8c945f34ae9725dffab2cc29eb10c5c6cf7
Aissamdev2 <shiranui201103@gmail.com>
2025-02-05 19:17:44 +0100

    sitemap

---
+++ next-sitemap.config.ts
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://gemif-web.vercel.app', // Change to your actual domain
+  generateRobotsTxt: true, // Generates a robots.txt file
+  sitemapSize: 5000, // Optional, limits number of URLs per sitemap file
+};
