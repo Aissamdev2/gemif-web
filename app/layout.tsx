e29fadc2ea8755347219a04ed534a74d91a014e5
Aissamdev2 <shiranui201103@gmail.com>
2025-02-05 12:22:32 +0100

    metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,13 +5,53 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Página web GEMiF, URV",
-  description: "Página web de GEMiF, Universitat Rovira i Virgili (URV) NO OFICIAL",
-  keywords: "Pagina web, Página web, web, gemif, GEMIF, GEMiF, URV, Universitat Rovira i Virgili",
+  title: "GEMiFWeb URV: Matemáticas y Física | Comunidad de Estudiantes",
+  description: "Pagina web de GEMiF, Universitat Rovira i Virgili (URV) NO OFICIAL",
+  keywords: "Pagina web, Página web, web, gemif, GEMIF, GEMiF, GEMIFWeb, gemifweb, matemáticas, fisica, matematicas, URV, Universitat Rovira i Virgili, estudiantes gemif",
+  applicationName: "GEMiFWeb URV",
   authors: [{ name: "Aissam Khadraoui" }],
-  icons: {
-    icon: "/favicon.ico",
-  }
+  openGraph: {
+    title: "GEMiFWeb URV: Matemáticas y Física | Comunidad de Estudiantes",
+    description: "Pagina web de GEMiF, Universitat Rovira i Virgili (URV) NO OFICIAL",
+    url: "https://gemif-web.vercel.app",
+    siteName: "GEMiFWeb URV",
+    locale: "es_ES",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "GEMiF URV: Recursos para Ingenieros Matemático-Físicos",
+    description: "Apuntes, tutoriales y apoyo académico para estudiantes de GEMiF en la URV",
+    images: ["https://your-project.vercel.app/twitter-image.jpg"],
+  },
+  icons: [
+    {
+      rel: "icon",
+      type: "image/ico",
+      sizes: "64x64",
+      url: "/favicon.ico",
+    },
+    {
+      rel: "apple-touch-icon",
+      sizes: "180x180",
+      type: "image/png",
+      url: "/apple-touch-icon.png",
+    },
+  ],
+  metadataBase: new URL("https://gemif-web.vercel.app"),
+  alternates: {
+    canonical: "/",
+  },
+  robots: {
+    index: true,
+    follow: true,
+    nocache: false,
+    googleBot: {
+      index: true,
+      follow: true,
+      noimageindex: false,
+    }
+  },
 };
 
 export default function RootLayout({
