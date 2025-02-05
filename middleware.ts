eef2f13d7577219b7c93d125e424722daae7b09b
Aissamdev2 <shiranui201103@gmail.com>
2025-02-05 19:40:16 +0100

    sitemaps

--- middleware.ts
+++ middleware.ts
@@ -64,7 +64,7 @@ export async function middleware(request: NextRequest) {
 
 export const config = {
   matcher: [
-    '/((?!_next/static|_next/image|.*\\.(?:png|jpg|jpeg|ico|svg|xml)$).*)',
+    '/((?!_next/static|_next/image|.*\\.(?:png|jpg|jpeg|ico|svg|xml|txt)$).*)',
     '/api/(.*)',
   ],
 };
