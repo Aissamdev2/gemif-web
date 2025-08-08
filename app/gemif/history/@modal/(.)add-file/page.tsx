6fd3bbadc62a8ffa046c6a4bd45add14a70a1a16
Aissamdev2 <shiranui201103@gmail.com>
2025-08-08 18:39:59 +0200

    useSearchParams wrapped in suspense

--- app/gemif/history/@modal/(.)add-file/page.tsx
+++ app/gemif/history/@modal/(.)add-file/page.tsx
@@ -1,17 +1,29 @@
 'use client'
 
-
 import Modal from "@/app/ui/modal"
 import AddFileForm from "@/app/ui/add-file-form"
 import { useSearchParams } from "next/navigation";
+import { Suspense } from "react";
 
-export default function AddFilePage() {
+function AddFile() {
   const searchParams = useSearchParams();
   const fullPath = decodeURIComponent(searchParams.get("path") || "");
 
   return (
     <Modal>
       <AddFileForm fullPath={fullPath}  />
+    </Modal>
+  )
+}
+
+
+export default function AddFilePage() {
+
+  return (
+    <Modal>
+      <Suspense>
+        <AddFile />
+      </Suspense>
     </Modal>
   )
 }
--- app/gemif/history/add-folder/page.tsx
+++ app/gemif/history/add-folder/page.tsx
@@ -3,14 +3,27 @@
 import Modal from "@/app/ui/modal"
 import AddFolderForm from "@/app/ui/add-folder-form"
 import { useSearchParams } from "next/navigation";
+import { Suspense } from "react";
 
-export default function AddFolderPage() {
+function AddFolder() {
   const searchParams = useSearchParams();
   const fullPath = decodeURIComponent(searchParams.get("path") || "");
 
   return (
     <Modal>
       <AddFolderForm fullPath={fullPath}  />
+    </Modal>
+  )
+}
+
+
+export default function AddFolderPage() {
+
+  return (
+    <Modal>
+      <Suspense>
+        <AddFolder />
+      </Suspense>
     </Modal>
   )
 }
