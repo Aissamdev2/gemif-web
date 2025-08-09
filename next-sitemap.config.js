74298bdf897f8a6a0b61c5d6e3bac071031333b1
Aissamdev2 <shiranui201103@gmail.com>
2025-08-09 17:32:34 +0200

    url change

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: 'https://gemif-web.vercel.app', // Change to your actual domain
+  siteUrl: 'https://gemif.es', // Change to your actual domain
   generateRobotsTxt: true, // Generates a robots.txt file
   sitemapSize: 5000, // Optional, limits number of URLs per sitemap file
 };
