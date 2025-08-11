d498734e28cc0ee53ac7e4fc8aeea7c6b77644f0
Aissamdev2 <shiranui201103@gmail.com>
2025-08-11 13:58:42 +0200

    analytics and speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -77,7 +79,12 @@ export default function RootLayout({
         <JsonLdSiteNavigation />
         
       </head>
-      <body className={inter.className + " h-screen"}>{children}</body>
+      
+      <body className={inter.className + " h-screen"}>
+        {children}
+        <Analytics />
+        <SpeedInsights />
+      </body>
     </html>
   );
 }
